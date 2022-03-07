875fe27372fdcb91dde9ce14f1977bea5d839134
Michael Kendrick <michael@locumsmart.net>
2022-03-07 16:13:35 -0600

    trying to pull lat and long

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,33 +18,34 @@ function formSubmitHandler(event) {
   }
 }
 
+function getLatLon() {
+  var getLat =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    cityInputEl.value +
+    "&limit=1&appid=c87ce505ce52817e75d4e9f4b1e2a56a";
+  fetch(getLat).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        var latitude = data[0].lat;
+      });
+    }
+  });
+
+  var getLon =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    cityInputEl.value +
+    "&limit=1&appid=c87ce505ce52817e75d4e9f4b1e2a56a";
+  fetch(getLon).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        var longitude = data[0].lon;
+      });
+    }
+  });
+}
+
 function getCityWeather(city) {
-  //   var apiUrl =
-  //     "https://api.openweathermap.org/data/2.5/onecall?lat=36.1659&lon=-86.7844&appid=c87ce505ce52817e75d4e9f4b1e2a56a";
-
-  var latitude =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    cityInputEl.value +
-    "&limit=1&appid=c87ce505ce52817e75d4e9f4b1e2a56a";
-  fetch(latitude).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data.lat);
-      });
-    }
-  });
-
-  var longitude =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    cityInputEl.value +
-    "&limit=1&appid=c87ce505ce52817e75d4e9f4b1e2a56a";
-  fetch(longitude).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data.lon);
-      });
-    }
-  });
+  getLatLon();
   var apiUrl =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
     latitude +
@@ -55,7 +56,7 @@ function getCityWeather(city) {
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
+        getLatLon();
         displayCity(data);
       });
     } else {
