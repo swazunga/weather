6e681a18ca76600cadbd1143c6bdfb0e66628178
Michael Kendrick <michael@locumsmart.net>
2022-03-07 14:14:06 -0600

    getting help

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,35 @@
 var searchBtn = document.querySelector(".search-btn");
 var cityInputEl = document.querySelector("#city");
 var currentCityEl = document.querySelector(".current-city");
+var previousCityEl = document.querySelector(".previous-city");
 
 function formSubmitHandler(event) {
   event.preventDefault();
   var city = cityInputEl.value.trim();
   if (city) {
     getCityWeather(city);
+    // for (var i = 0; i < city.length; i++) {
+    // //   localStorage.setItem("event", city);
+    // //   var addToList = localStorage.getItem("event");
+    // //   previousCityEl.appendChild(addToList);
+    // }
   } else {
     alert("Error: Please enter a valid city");
   }
 }
 
 function getCityWeather(city) {
-  var apiUrl =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=36.1659&lon=-86.7844&appid=c87ce505ce52817e75d4e9f4b1e2a56a";
-
-  //   var latitude = data.lat;
-  //   var longitude = data.lon;
   //   var apiUrl =
-  //     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-  //     latitude +
-  //     "&lon=" +
-  //     longitude +
-  //     "&appid=c87ce505ce52817e75d4e9f4b1e2a56a";
+  //     "https://api.openweathermap.org/data/2.5/onecall?lat=36.1659&lon=-86.7844&appid=c87ce505ce52817e75d4e9f4b1e2a56a";
+
+  var latitude = data.lat;
+  var longitude = data.lon;
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    latitude +
+    "&lon=" +
+    longitude +
+    "&appid=c87ce505ce52817e75d4e9f4b1e2a56a";
 
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
@@ -38,9 +44,7 @@ function getCityWeather(city) {
 }
 
 function displayCity(data) {
-  var weatherInfoEl = document.createElement("p");
   currentCityEl.textContent = cityInputEl.value + "(" + Date() + ")";
-  currentCityEl.appendChild(weatherInfoEl);
   var currentTemp = document.createElement("p");
   currentTemp.textContent =
     "Temp: " +
