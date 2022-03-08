feff86c0d4dcf1d078eb969b669689e930a3fa2d
Michael Kendrick <michael@locumsmart.net>
2022-03-07 22:20:23 -0600

    getting help

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,42 +6,45 @@ var previousCityEl = document.querySelector(".previous-city");
 function formSubmitHandler(event) {
   event.preventDefault();
   var city = cityInputEl.value.trim();
-  getLatLon(city);
+  getLatLon(city).then(function (coord) {
+    getCityWeather(coord.lat, coord.lon);
+  });
   if (city) {
-    // for (var i = 0; i < city.length; i++) {
-    // //   localStorage.setItem("event", city);
-    // //   var addToList = localStorage.getItem("event");
-    // //   previousCityEl.appendChild(addToList);
-    // }
+    var previousCityItemEl = document.createElement("li");
+    var previousCityItemEl = document.createElement("button");
+    previousCityItemEl.classList = "list-previous btn-city";
+    previousCityItemEl.textContent = cityInputEl.value;
+    previousCityEl.prepend(previousCityItemEl);
   } else {
     alert("Error: Please enter a valid city!");
   }
 }
 
+function retrievePast(event) {
+  var prevCityStr = event.target.innerHTML;
+  getLanLon(prevCityStr);
+}
+
+previousCityEl.addEventListener("click", retrievePast);
+
 function getLatLon(city) {
-  fetch(
+  return fetch(
     "http://api.openweathermap.org/data/2.5/weather?q=" +
       city +
       "&appid=8d11ee23127ece86a8e45e07590427e7"
   ).then(function (response) {
     if (response.ok) {
-      response.json().then(function (data) {
+      return response.json().then(function (data) {
         console.log(data);
-        var lat = data.coord.lat;
-        var lon = data.coord.lon;
-        getCityWeather(lat, lon);
-        console.log(lat, lon);
-        return lat;
+        return data.coord;
       });
     }
   });
 }
-// console.log(lat);
-// console.log(lon);
 
 function getCityWeather(lat, lon) {
-  var latLon = getLatLon();
-  console.log(latLon);
+  //   var latLon = getLatLon(city);
+  //   console.log(latLon);
   var apiUrl =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
     lat +
@@ -61,7 +64,8 @@ function getCityWeather(lat, lon) {
 }
 
 function displayCity(data) {
-  currentCityEl.textContent = cityInputEl.value.trim() + "(" + Date() + ")";
+  currentCityEl.textContent =
+    cityInputEl.value.trim() + " (" + moment().format("MMMM Do, YYYY") + ")";
   var currentTemp = document.createElement("p");
   currentTemp.textContent =
     "Temp: " +
@@ -79,7 +83,6 @@ function displayCity(data) {
   uviEl.textContent = "UV Index: " + data.current.uvi;
   humidityEl.appendChild(uviEl);
   cityInputEl.textContent = "";
-  getCityWeather(city);
   console.log(data);
 }
 document.addEventListener("submit", formSubmitHandler);
