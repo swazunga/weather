17642c058f3f0b10ef4653ec2993ee4d7049e7a7
Michael Kendrick <michael@locumsmart.net>
2022-03-08 08:24:03 -0600

    getting help

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var searchBtn = document.querySelector(".search-btn");
 var cityInputEl = document.querySelector("#city");
 var currentCityEl = document.querySelector(".current-city");
 var previousCityEl = document.querySelector(".previous-city");
+var fiveDayEl = document.querySelector(".five-day");
 
 function formSubmitHandler(event) {
   event.preventDefault();
@@ -16,15 +17,49 @@ function formSubmitHandler(event) {
     previousCityItemEl.textContent = cityInputEl.value;
     previousCityEl.prepend(previousCityItemEl);
   } else {
-    alert("Error: Please enter a valid city!");
+    alert("Error: Please enter a valid city");
   }
+  //   formSubmitHandler.catch(alert("Please enter a valid city name"));
 }
 
 function retrievePast(event) {
   var prevCityStr = event.target.innerHTML;
   currentCityEl.textContent = prevCityStr;
   getLatLon(prevCityStr).then(function (coord) {
-    getCityWeather(coord.lat, coord.lon);
+    var apiUrl =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      coord.lat +
+      "&lon=" +
+      coord.lon +
+      "&appid=8d11ee23127ece86a8e45e07590427e7";
+    console.log(apiUrl);
+    fetch(apiUrl).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          currentCityEl.textContent =
+            prevCityStr + " (" + moment().format("MMMM Do, YYYY") + ")";
+          var currentTemp = document.createElement("p");
+          currentTemp.textContent =
+            "Temp: " +
+            (Math.round((data.current.temp - 273.15) * (9 / 5) + 32) * 100) /
+              100 +
+            "°F";
+          currentCityEl.appendChild(currentTemp);
+          var windEl = document.createElement("p");
+          windEl.textContent = "Wind: " + data.current.wind_speed + " MPH";
+          currentTemp.appendChild(windEl);
+          var humidityEl = document.createElement("p");
+          humidityEl.textContent = "Humidity: " + data.current.humidity + " %";
+          windEl.appendChild(humidityEl);
+          var uviEl = document.createElement("p");
+          uviEl.classList = "uvi";
+          uviEl.textContent = "UV Index: " + data.current.uvi;
+          humidityEl.appendChild(uviEl);
+        });
+      } else {
+        alert("Error: Please enter a valid city");
+      }
+    });
   });
 }
 
@@ -68,7 +103,7 @@ function getCityWeather(lat, lon) {
 
 function displayCity(data) {
   currentCityEl.textContent =
-    cityInputEl.value.trim() + " (" + moment().format("MMMM Do, YYYY") + ")";
+    cityInputEl.value + " (" + moment().format("MMMM Do, YYYY") + ")";
   var currentTemp = document.createElement("p");
   currentTemp.textContent =
     "Temp: " +
@@ -85,7 +120,39 @@ function displayCity(data) {
   uviEl.classList = "uvi";
   uviEl.textContent = "UV Index: " + data.current.uvi;
   humidityEl.appendChild(uviEl);
-  cityInputEl.textContent = "";
-  console.log(data);
+  cityInputEl.value = "";
+  fiveDay(data);
+}
+
+function fiveDay(data) {
+  for (var i = 0; i < 5; i++) {
+    var daily = data.daily[i];
+    console.log(daily);
+
+    var eachDayEl = document.createElement("p");
+    var iconEl = document.createElement("p");
+    iconEl.classList = "";
+    iconEl.textContent = daily.weather[0].icon;
+    console.log(daily.weather[0].icon);
+    fiveDayEl.appendChild(iconEl);
+    iconEl.appendChild(eachDayEl);
+    eachDayEl.textContent = new Date().toDateString();
+    var currentTemp = document.createElement("p");
+    currentTemp.textContent =
+      "Temp: " +
+      (Math.round((daily.temp.day - 273.15) * (9 / 5) + 32) * 100) / 100 +
+      "°F";
+    eachDayEl.appendChild(currentTemp);
+    var windEl = document.createElement("p");
+    windEl.textContent = "Wind: " + data.current.wind_speed + " MPH";
+    currentTemp.appendChild(windEl);
+    var humidityEl = document.createElement("p");
+    humidityEl.textContent = "Humidity: " + data.current.humidity + " %";
+    windEl.appendChild(humidityEl);
+    var uviEl = document.createElement("p");
+    uviEl.classList = "uvi";
+    uviEl.textContent = "UV Index: " + data.current.uvi;
+    humidityEl.appendChild(uviEl);
+  }
 }
 document.addEventListener("submit", formSubmitHandler);
